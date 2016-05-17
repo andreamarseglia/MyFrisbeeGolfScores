6f1e4e26961bc3f6736edffbe327f553d218fa1a
andreamarseglia <marseglia.a@gmail.com>
2016-05-17 20:06:24 +0300

    Initial commit

---
+++ app/src/main/java/andreamarseglia/com/myfrisbeegolfscores/MenuFragment.java
@@ -0,0 +1,39 @@
+package andreamarseglia.com.myfrisbeegolfscores;
+
+import android.app.ListFragment;
+import android.widget.AdapterView;
+import android.annotation.SuppressLint;
+import android.app.ListFragment;
+import android.os.Bundle;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
+/**
+ * Created by Andrea on 17.5.2016.
+ */
+public class MenuFragment extends ListFragment implements AdapterView.OnItemClickListener{
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        View view = inflater.inflate(R.layout.list_fragment, container, false);
+        return view;
+    }
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState){
+        super.onActivityCreated(savedInstanceState);
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(getActivity(),R.array.Menu,R.layout.list_item);
+        setListAdapter(adapter);
+        getListView().setOnItemClickListener(this);
+    }
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id){
+        Toast.makeText(getActivity(), ListItem, Toast.LENGTH_SHORT).show();
+    }
+
+}
